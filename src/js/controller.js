822ca3ef7d3aff86d6cb648a4598a94d28eb4a8d
07nazar <ecsta3452@gmail.com>
2023-01-03 00:53:50 +0200

    сделать логику для последних постов

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,25 +1,46 @@
-import * as model from "./model";
-import bestProductsView from "./views/bestProductsView";
+import * as model from './model';
+import bestProductsView from './views/bestProductsView';
+import postsView from './views/postsView';
 
 const bestSellersController = async () => {
-  try {
-    bestProductsView.showLoader();
+	try {
+		bestProductsView.showLoader();
 
-    const products = await model.fetchProducts({
-      fetchLimit: 4,
-      fieldPath: "rating",
-      opStr: ">=",
-      value: 4.9,
-    });
+		const products = await model.fetchItems({
+			fetchLimit: 4,
+			fieldPath: 'rating',
+			fieldSort: 'rating',
+			directionStr: 'desc',
+			opStr: '>=',
+			value: 4.9,
+			collectionType: 'products',
+		});
 
-    bestProductsView.render(products);
-  } catch (error) {
-    bestProductsView.showError();
-  }
+		bestProductsView.render(products);
+	} catch (error) {
+		bestProductsView.showError();
+	}
+};
+
+const latestPostsController = async () => {
+	try {
+		postsView.showLoader();
+		const posts = await model.fetchItems({
+			fetchLimit: 3,
+			collectionType: 'posts',
+			fieldSort: 'date',
+			directionStr: 'desc',
+		});
+
+		postsView.render(posts);
+	} catch (error) {
+		postsView.showError();
+	}
 };
 
 const init = () => {
-  bestProductsView.loadHandler(bestSellersController);
+	bestProductsView.loadHandler(bestSellersController);
+	postsView.loadHandler(latestPostsController);
 };
 
 init();
