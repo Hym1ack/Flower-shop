c19b0ebbe32db91e5b8a993983c0bd8e7ba34d7b
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-01-02 18:38:51 +0300

    Отображение блока best selers

---
+++ src/js/views/bestProductsView.js
@@ -0,0 +1,31 @@
+import View from "./view";
+
+class BestProductsView extends View {
+  _parent = document.querySelector(".best-sellers_products");
+
+  _error = "Products is 404";
+
+  loadHandler(handler) {
+    if (!this._parent) return;
+
+    handler();
+  }
+
+  _createLayout() {
+    return this._data.map(
+      (product) =>
+        `<div class="product__card">
+        <a href="./pages/Product/product.html" class="product__card-link">
+            <img class="product__card-img" src=${product.photo} alt=${product.title}>
+        </a>
+        <h5 class="product__card-title">${product.title}</h5>
+        <div class="product__card-shop">
+          <div class="product__card-price">${product.price}$</div>
+          <button class="product__card-add">Add to cart</button>
+        </div>
+      </div>`
+    );
+  }
+}
+
+export default new BestProductsView();
